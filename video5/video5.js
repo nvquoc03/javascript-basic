c9e880e3480f411ae2df02ecb60b93a677904e74
Van Quoc <quocx2003@gmail.com>
2023-01-16 15:02:25 +0700

    chapter2 video5

---
+++ video5/video5.js
@@ -0,0 +1,6 @@
+let txt = "VANQUOC";
+
+console.log(txt.slice(1,7))
+
+console.log("Dùng GG dịch thứ cần tìm và seach bằng tiếng Anh; Method là hàm or phương pháp")
+console.log(`Học thứ cần thiết, nếu gặp thứ k biết thì lên "W3school" `)
