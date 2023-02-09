9729a697947e76c25208d07fc37fbd49dfdc421e
nvquoc03 <122554090+nvquoc03@users.noreply.github.com>
2023-02-09 21:21:47 +0700

    Update video24.js

--- video24/video24.js
+++ video24/video24.js
@@ -8,7 +8,7 @@ let sorter = arr.sort((previousValue,currentValue)=>{ //previousValue là phần
 // nếu a>0 -> b trước a ... nếu a<0 -> a trước b =>> Dùng để xác định xem số vào là previous và số nào là current, xog đó mới tính a-b (Không chắc chắn, chỉ là suy luận cá nhân, nên bám theo bài cũ)
 
 // VD: a = previousValue, b = currentValue 
-//---> a-b<= thì a trước b, a-b>0 thì a sau b
+//---> a-b<0 thì a trước b, a-b>0 thì a sau b
 console.log("Check sort: ",sorter); // Đây là hàm sort sắp xếp các số lớn dần theo giá trị
 
 
